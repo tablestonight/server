8af1069d54eeefdee4cc2b153e338246e060bcbe
Justin Pinili <pinili.justin@gmail.com>
2016-02-18 21:44:51 -0800

    complete use of promises on the back end

--- database/hosts/hosts.database.js
+++ database/hosts/hosts.database.js
@@ -7,126 +7,171 @@
 	module.exports.loginHost  = loginHost;
 	module.exports.updateHost = updateHost;
 
-	function createHost(info, response) {
-		var newHost = new schema.Hosts({
-			firstName: '',
-			lastName: '',
-			nightClub: '',
-			mobile: '',
-			email: info.email,
-			password: info.password,
-			dayClub: '',
-			nickName: '',
-			description: '',
-			socialMedia: {
-				instagram: '',
-				twitter: ''
+	function createSalt() {
+		var deferred = Q.defer();
+		bcrypt.genSalt(10, function(error, salt) {
+			if (error) {
+				return deferred.reject(error);
 			}
+			return deferred.resolve(salt);
 		});
+		return deferred.promise;
+	}
+
+	function hashPassword(salt, info) {
+		var deferred = Q.defer();
+		bcrypt.hash(info.password, salt, function(error, hash) {
+			if (error) {
+				return deferred.reject(error);
+			}
+			return deferred.resolve(hash);
+		});
+		return deferred.promise;
+	}
+
+	function createHost(info, response) {
+
+		var newHost = createNewHost(schema);
 
 		createSalt()
 			.then(function(salt) {
-				return hashPassword(salt);
+				return hashPassword(salt, info);
 			})
 			.then(function(hash) {
-				newHost.password = hash;
-				return saveNewHost();
+				return saveNewHost(hash);
 			})
 			.then(function(newHost) {
 				return response.send(newHost);
 			})
 			.fail(function(error) {
 				return response.send(error);
-			})
+			});
 
-		function saveNewHost() {
+		///////////////////////////////////////////////////////////////////////////
+
+		function createNewHost(schema) {
+			return new schema.Hosts({
+				firstName: '',
+				lastName: '',
+				nightClub: '',
+				mobile: '',
+				email: info.email,
+				password: info.password,
+				dayClub: '',
+				nickName: '',
+				description: '',
+				socialMedia: {
+					instagram: '',
+					twitter: ''
+				}
+			});
+		}
+
+		function saveNewHost(hash) {
 			var deferred = Q.defer();
+			newHost.password = hash;
 			newHost.save(function (error) {
 				if (error) {
-					deferred.reject(error);
-				} else {
-					deferred.resolve(newHost);
+					if (error.code === 11000) {
+						return deferred.reject('e-mail already taken. please use a different e-mail address.');
+					}
+					return deferred.reject(error);
 				}
+				return deferred.resolve(newHost);
 			});
 			return deferred.promise;
 		}
+	}
 
-		function hashPassword(salt) {
-			var deferred = Q.defer();
-			bcrypt.hash(info.password, salt, function(error, hash) {
-				if (error) {
-					deferred.reject(error);
-				} else {
-					deferred.resolve(hash);
-				}
-			});
-			return deferred.promise;
-		}
-
-		function createSalt() {
-			var deferred = Q.defer();
-			bcrypt.genSalt(10, function(error, salt) {
-				if (error) {
-					deferred.reject(error);
-				} else {
-					deferred.resolve(salt);
-				}
-			});
-			return deferred.promise;
-		}
-	};
-
-	function loginHost(info, response) {
+	function findExistingHost(info) {
+		var deferred = Q.defer();
 		schema.Hosts.where({ email: info.email}).findOne(function(error, host) {
 			if (error) {
-				response.send(error);
-				return;
+				return deferred.reject(error);
 			}
-			bcrypt.compare(info.password, host.password, function(error, result) {
-				if (error) {
-					response.send(error);
-					return;
-				}
-				if (result) {
-					response.send(host);
-					return;
-				}
-				response.send("Incorrect credentials. Please try again.");
-			});
+			if (host) {
+				return deferred.resolve(host);
+			}
+			return deferred.reject('E-mail not found. Please try again.');
 		});
+		return deferred.promise;
+	}
+
+	function loginHost(info, response) {
+
+		findExistingHost(info)
+			.then(function(host) {
+				return verifyPassword(host, info)
+			})
+			.then(function(host) {
+				response.send(host);
+			})
+			.fail(function(error) {
+				response.send(error);
+			});
+
+		///////////////////////////////////////////////////////////////////////////
+
+		function verifyPassword(host, info) {
+			var deferred = Q.defer();
+			bcrypt.compare(info.password, host.password, function(error, passwordMached) {
+				if (error) {
+					return deferred.reject(error);
+				}
+				if (passwordMached) {
+					return deferred.resolve(host);
+				}
+				return deferred.reject('Incorrect credentials. Please try again.')
+			});
+			return deferred.promise;
+		}
 	};
 
 	function updateHost(info, response) {
-		schema.Hosts.where({ email: info.email}).findOne(function(error, host) {
-			if (error) {
-				response.send(error);
-				return;
-			}
-			bcrypt.genSalt(10, function(error, salt) {
-				bcrypt.hash(info.password, salt, function(err, hash) {
-					if (error) {
-						response.send(error);
-						return;
-					}
-					host.password    = hash;
-					host.firstName   = info.firstName;
-					host.lastName    = info.lastName;
-					host.nightClub   = info.nightClub;
-					host.mobile      = info.mobile;
-					host.email       = info.email;
-					host.dayClub     = info.dayClub;
-					host.nickName    = info.nickName;
-					host.description = info.description;
-					host.socialMedia = info.socialMedia;
-					host.save(function (error, updatedHost) {
-						if (error) {
-							response.send(error);
-							return;
-						}
-						response.send(updatedHost);
-					});
-				});
+
+		var host = null;
+
+		findExistingHost(info)
+			.then(function(foundHost) {
+				host = foundHost;
+				return createSalt();
+			})
+			.then(function(salt) {
+				return hashPassword(salt, info);
+			})
+			.then(function(hash) {
+				return updateHost(hash, info, host);
+			})
+			.then(function(updatedHost) {
+				return response.send(updatedHost);
+			})
+			.fail(function(error) {
+				return response.send(error);
 			});
-		});
+
+		///////////////////////////////////////////////////////////////////////////
+
+		function updateHost(hash, info, host) {
+			var deferred = Q.defer();
+			host.password    = hash;
+			host.firstName   = info.firstName;
+			host.lastName    = info.lastName;
+			host.nightClub   = info.nightClub;
+			host.mobile      = info.mobile;
+			host.email       = info.email;
+			host.dayClub     = info.dayClub;
+			host.nickName    = info.nickName;
+			host.description = info.description;
+			host.socialMedia = info.socialMedia;
+
+			host.save(function (error) {
+				if (error) {
+					return deferred.reject(error);
+				}
+				return deferred.resolve(host);
+			});
+			return deferred.promise;
+		}
+
 	};
 })();
--- database/hosts/hosts.schema.js
+++ database/hosts/hosts.schema.js
@@ -1,38 +1,40 @@
-var mongoose = require("mongoose");
+(function() {
+	var mongoose = require("mongoose");
 
-var hostSchema = mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	nightClub: String,
-	mobile: {
-		type: String,
-		unique: true
-	},
-	email: {
-		type: String,
-		unique: true
-	},
-	password: String,
-	dayClub: String,
-	nickName: String,
-	description: String,
-	socialMedia: {},
-});
+	var hostSchema = mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		nightClub: String,
+		mobile: {
+			type: String,
+			unique: true
+		},
+		email: {
+			type: String,
+			unique: true
+		},
+		password: String,
+		dayClub: String,
+		nickName: String,
+		description: String,
+		socialMedia: {},
+	});
 
-module.exports.Hosts = mongoose.model('Hosts', hostSchema);
+	module.exports.Hosts = mongoose.model('Hosts', hostSchema);
 
-// {
-// 	"firstName": "Justin",
-// 	"lastName": "Jrobot",
-// 	"nightClub": "XS",
-// 	"mobile": "1333333337",
-// 	"email": "jro@bot.com",
-// 	"password": "lebron",
-// 	"dayClub": "Wet Republic",
-// 	"nickName": "jro_bot",
-// 	"description": "ima robot",
-// 	"socialMedia": {
-// 		"twitter": "@jrobot",
-// 		"instagram": "@jrobot"
-// 	}
-// }
+	// {
+	// 	"firstName": "Justin",
+	// 	"lastName": "Jrobot",
+	// 	"nightClub": "XS",
+	// 	"mobile": "1333333337",
+	// 	"email": "jro@bot.com",
+	// 	"password": "lebron",
+	// 	"dayClub": "Wet Republic",
+	// 	"nickName": "jro_bot",
+	// 	"description": "ima robot",
+	// 	"socialMedia": {
+	// 		"twitter": "@jrobot",
+	// 		"instagram": "@jrobot"
+	// 	}
+	// }
+})();
